b28edc30b2b747e1f2b2f716145b9f474d7903e4
Bohdan Poberezhnyi <bogdanpoberezhnyi@gmail.com>
2024-11-30 15:36:23 +0100

    added resources

--- src/main/java/cz/cvut/fit/niadp/mvcgame/config/MvcGameResources.java
+++ src/main/java/cz/cvut/fit/niadp/mvcgame/config/MvcGameResources.java
@@ -4,4 +4,10 @@ public class MvcGameResources {
 
     public static final String CANNON_RESOURCE = "images/cannon.png";
     public static final String MISSILE_RESOURCE = "images/missile.png";
+    public static final String BACKGROUND_RESOURCE = "images/back.jpg";
+    public static final String BOUND_RESOURCE = "images/bound.png";
+    public static final String COLLISION_RESOURCE = "images/collision.png";
+    public static final String ENEMY_1_RESOURCE = "images/enemy1.png";
+    public static final String ENEMY_2_RESOURCE = "images/enemy2.png";
+    public static final String ENEMY_2_WITH_BLOOD_RESOURCE = "images/enemy2WithBlood.png";
 }
